c3f38a5583a7c7c9022b953145c588e3ea90d685
YangMun <53438709+YangMun@users.noreply.github.com>
2022-04-21 16:26:16 +0900

    Use_OnlyBolllingerBand

--- GitHub_Upload/Main_BollingerBand.py
+++ GitHub_Upload/Main_BollingerBand.py
@@ -78,12 +78,6 @@ def get_BollingerBand(market):
     return df
     
 
-def get_target_price(market, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(market, interval="day", count=2)
-    target_price = df.iloc[1]['open'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
-    return target_price
-
 def get_start_time(market):
     """시작 시간 조회"""
     df = pyupbit.get_ohlcv(market, interval="day", count=1)
@@ -116,42 +110,36 @@ while True:
         now = datetime.datetime.now()
         start_time = get_start_time(get_momentum(1)) # 09:00
         end_time = start_time + datetime.timedelta(days=1) # 다음 날 09:00
-        
-        if start_time == now:
-            dbu = DBUpdater.DBUpdater()
-            dbu.execute_daily()
     
         if start_time < now < end_time - datetime.timedelta(seconds=15):
-            market = get_momentum(100)
+            market = get_momentum(107)
             """market 개수인 10번을 반복하여 매수 매매 반복 시킨다 (10만원 까지만 사용 가능하도록 한다.)"""
             for mk in range(len(market)):
                 df = get_BollingerBand(market[mk])
-                target_price = np.array(get_target_price(market[mk], 0.7))
                 current_price = get_current_price(market[mk])
                 slice_market = market[mk].split('-')[1]  # KRW- 뒤에 부분만 얻기 위함
-                print(f"현재 가격 :{current_price} ... 매수 목표가 : {target_price}")
-                if target_price < current_price:
-                    krw = get_balance("KRW")
-                    buy_krw = get_balance(slice_market)
-                    """내 잔고가 5천원 이상이고 매수 한 종목의 잔고가 10만원 이하일 때까지"""
+                krw = get_balance("KRW")
+                buy_krw = get_balance(slice_market)
+                """내 잔고가 5천원 이상이고 매수 한 종목의 잔고가 10만원 이하일 때까지"""
+                if df.PB.values[mk] > 0.8 and df.MFI10.values[mk] > 80:
                     if buy_krw * current_price <= 250000:
                         upbit.buy_market_order(market[mk], krw*0.139)
                         print(f"{market[mk]} 구매, {buy_krw}현재 보유 금액")
-                    if df.PB.values[mk] > 0.8 and df.MFI10.values[mk] > 80:
-                        upbit.buy_market_order(market[mk], krw*0.139)
-                        print(f"{market[mk]} 구매, {buy_krw}현재 보유 금액")
-                    elif df.PB.values[mk] < 0.2 and df.MFI10.values[mk] < 20:
-                        upbit.sell_market_order(market[mk], buy_krw*0.139)
-                        print(f"{market[mk]} 매도, 남은 금액 {buy_krw}")
-                    else:
-                        continue
+                elif df.PB.values[mk] < 0.2 and df.MFI10.values[mk] < 20:
+                    upbit.sell_market_order(market[mk], buy_krw*0.139)
+                    print(f"{market[mk]} 매도, 남은 금액 {buy_krw}")
                 else:
                     continue
+                        
+        elif start_time == now:
+            dbu = DBUpdater.DBUpdater()
+            dbu.execute_daily()
+            
         else:
             for bk in range(len(market)):
                 slice_market = market[bk].split('-')[1]  # KRW- 뒤에 부분만 얻기 위함
                 buy_krw = get_balance(slice_market)
-                if buy_krw >0.0000001:
+                if df.PB.values[bk] < 0.2 and df.MFI10.values[bk] < 20:
                     upbit.sell_market_order(market[bk], buy_krw*0.139)
                     print(f"{market[bk]} 매도, 남은 금액 {buy_krw}")
                 else:
